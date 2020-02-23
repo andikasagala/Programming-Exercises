eb1ae0941adee6341e5673f56b1dcb007c36411f
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-02-23 19:24:07 +0700

    Programming Exercises
    Programming Exercises

---
+++ C#/Hello-World-Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+
+}
