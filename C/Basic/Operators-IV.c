8f5d489f51b62424ac062a2df113023ea87defb1
Andika Sagala <andy@localhost.localdomain>
2020-03-01 15:09:59 -0500

    Programming Exercise

--- C/Basic/Operators-IV.c
+++ C/Basic/Operators-IV.c
@@ -24,6 +24,18 @@ int VariableF=8;
 int VariableG=9;
 int VariableH=10;
 int VariableI=5;
+int VariableJ=10;
+int VariableK=3;
+int VariableL=2;
+int VariableM=4;
+int VariableN=3;
+int VariableO=8;
+int VariableP=2;
+int VariableQ=4;
+int VariableR=3;
+int VariableS=5;
+int VariableT=7;
+int VariableU=2;
 
   
 printf("___________________\n");
@@ -37,6 +49,20 @@ printf("Variable F value is: 8 \n");
 printf("Variable G value is: 9 \n");
 printf("Variable H value is: 10 \n");
 printf("Variable I value is: 5 \n");
+printf("Variable J value is: 10 \n");
+printf("Variable K value is: 3 \n");
+printf("Variable L value is: 2 \n");
+printf("Variable M value is: 4 \n");
+printf("Variable N value is: 3 \n");
+printf("Variable O value is: 8 \n");
+printf("Variable P value is: 2 \n");
+printf("Variable Q value is: 4 \n");
+printf("Variable R value is: 3 \n");
+printf("Variable S value is: 5 \n");
+printf("Variable T value is: 7 \n");
+printf("Variable U value is: 2 \n");
+
+
 printf("====================\n");
 
 printf("Operator Assignment ( = ) Variable A = 5. Calculation value: %d \n", VariableA);
@@ -44,6 +70,12 @@ printf("Operator Assignment ( += ) Variable B = Variable B + Variable C. Calcula
 printf("Operator Assignment ( -= ) Variable D = Variable D - Variable E. Calculation value: %d \n", VariableD-=VariableE);
 printf("Operator Assignment ( *= ) Variable F = Variable F * Variable G. Calculation value: %d \n", VariableF*=VariableG);
 printf("Operator Assignment ( /= ) Variable H = Variable H / Variable I. Calculation value: %d \n", VariableH/=VariableI);
+printf("Operator Assignment ( %= ) Variable J = Variable J % Variable K. Calculation value: %d \n", VariableJ%=VariableK);
+printf("Operator Assignment ( &= ) Variable L = Variable L & Variable M. Calculation value: %d \n", VariableL&=VariableM);
+printf("Operator Assignment ( |= ) Variable N = Variable N | Variable O. Calculation value: %d \n", VariableN|=VariableO);
+printf("Operator Assignment ( ^= ) Variable P = Variable P ^ Variable Q. Calculation value: %d \n", VariableP^=VariableQ);
+printf("Operator Assignment ( <<= ) Variable R = Variable R << Variable S. Calculation value: %d \n", VariableR<<=VariableS);
+printf("Operator Assignment ( >>= ) Variable T = Variable T >> Variable U. Calculation value: %d \n", VariableT>>=VariableU);
     
      
     
