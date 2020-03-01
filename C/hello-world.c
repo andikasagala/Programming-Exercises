721840e6f7a142d7061a99040809dab9610b4cdf
Andika Sagala <andy@localhost.localdomain>
2020-03-01 10:07:53 -0500

    Programming Exercises

---
+++ C/hello-world.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+#############################################################
+#                                                           #
+#           	---== Hello World ==---	                    #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2019                           #
+#############################################################
+*/
+
+
+int main ()
+{
+printf("Hello World!!!\n");
+
+return 0;   
+}
