800225624fab6c89320eff160cf28aca31f057a9
Andika Sagala <andy@localhost.localdomain>
2020-03-01 12:55:50 -0500

    Programming Exercise

---
+++ C/Operators-IV.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+#############################################################
+#                                                           #
+#         ---== Operators IV Assignment ==---              #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2020                           #
+#############################################################
+*/
+
+
+int main ()
+{
+int VariableA=5;
+int VariableB=3;
+int VariableC=4;
+int VariableD=6;
+int VariableE=7;
+int VariableF=8;
+int VariableG=9;
+int VariableH=10;
+int VariableI=5;
+
+  
+printf("___________________\n");
+printf("Operators Assignment!!!\n");
+printf("Variable A value is: 5 \n");
+printf("Variable B value is: 3 \n");
+printf("Variable C value is: 4 \n");
+printf("Variable D value is: 6 \n");
+printf("Variable E value is: 7 \n");
+printf("Variable F value is: 8 \n");
+printf("Variable G value is: 9 \n");
+printf("Variable H value is: 10 \n");
+printf("Variable I value is: 5 \n");
+printf("====================\n");
+
+printf("Operator Assignment ( = ) Variable A = 5. Calculation value: %d \n", VariableA);
+printf("Operator Assignment ( += ) Variable B = Variable B + Variable C. Calculation value: %d \n", VariableB+=VariableC);
+printf("Operator Assignment ( -= ) Variable D = Variable D - Variable E. Calculation value: %d \n", VariableD-=VariableE);
+printf("Operator Assignment ( *= ) Variable F = Variable F * Variable G. Calculation value: %d \n", VariableF*=VariableG);
+printf("Operator Assignment ( /= ) Variable H = Variable H / Variable I. Calculation value: %d \n", VariableH/=VariableI);
+    
+     
+    
+return 0;   
+}
