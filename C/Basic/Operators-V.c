fc8d08165b172e0dfce85e5d0558730cf1775c6d
Andika Sagala <andy@localhost.localdomain>
2020-03-01 14:44:28 -0500

    Programming Exercise

--- C/Basic/Operators-V.c
+++ C/Basic/Operators-V.c
@@ -18,15 +18,53 @@ int main ()
 
 int a=1;
 int b=1;
+int c;
+printf("====================== \n");
+printf("Variable a value is %d \n", a);
+printf("Variable b value is %d \n", b);
+printf("====================== \n");
 
-if(a & b)
+printf("______________________________________\n");
+if(~c)
 {
-printf("true");
+c=a+b;
+printf("true \n");
+printf("Operators Bitwise ( ~ ) c value is %d \n", c);
 }
 else
 {
-    printf("false");
+printf("false\n");
 }
+printf("______________________________________\n");
+
+
+if(a & b==2)
+{
+
+printf("true \n");
+printf("Operators Bitwise ( & ) a value = %d and b value = %d \n", a,b);
+}
+else
+{
+printf("false\n");
+printf("Operators Bitwise ( & ) a value = 1 and b value = 1 \n");
+}
+    
+printf("______________________________________\n");
+if( a==10 | b )
+{
+
+printf("true \n");
+printf("Operators Bitwise ( | ) a value = %d or b value = %d \n", a,b);
+}
+else
+{
+printf("false");
+}
+    
+printf("______________________________________\n");
+    
+    
     
 return 0;   
 }
