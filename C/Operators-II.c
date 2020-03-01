4d3ea7d804462e64f96c6559a0bbef3a8d060fc5
Andika Sagala <andy@localhost.localdomain>
2020-03-01 12:16:32 -0500

    Programming Exercise

---
+++ C/Operators-II.c
@@ -0,0 +1,129 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+#############################################################
+#                                                           #
+#         ---== Operators II Relational ==---                #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2020                           #
+#############################################################
+*/
+
+
+int main ()
+{
+int VariableA=1;
+int VariableB=2;
+  
+printf("___________________\n");
+printf("Operators Relational!!!\n");
+printf("====================\n");
+if( VariableA < VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( < ). VariableA=1 < Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( < ). VariableA=1 < Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+if( VariableA <= VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( <= ). VariableA=1 <= Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( <= ). VariableA=1 <= Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+    
+
+if( VariableA > VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( > ). VariableA=1 > Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( > ). VariableA=1 > Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+    
+     
+
+if( VariableA >= VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( >= ). VariableA=1 >= Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( >= ). VariableA=1 >= Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+     
+
+if( VariableA == VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( == ). VariableA=1 == Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( == ). VariableA=1 == Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+    
+
+if( VariableA != VariableB)
+{
+    printf ("True\n");
+    printf("Operators Relational ( != ). VariableA=1 != Variable=2 \n");
+    printf("______________________________________________________\n");
+
+}
+else
+    {
+        printf("False\n");
+        printf("Operators Relational ( != ). VariableA=1 != Variable=2 \n");
+        printf("______________________________________________________\n");
+    }
+    
+
+    
+     
+     
+    
+    
+return 0;   
+}
