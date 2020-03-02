a011c60fc6ca7375e9c41b19f62d0465de00f3f0
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-02 19:17:13 +0700

    Programming Exercises
    Programming Exercises

---
+++ C/basic/loop-III.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+#############################################################
+#                                                           #
+# 		---== Loop III while, do ==---		    #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2020                           #
+#############################################################
+*/
+
+int main()
+{
+//declare the variable
+int i=0;
+
+//looping forever using while
+do
+{
+printf("hola \n");
+}while(i<1);
+
+
+return 0;
+}
+
