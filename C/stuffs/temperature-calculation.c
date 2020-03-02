b38afb959e9c440cab26aa02f201235a8c32b779
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-02 10:06:56 +0700

    Programming Exercises
    Programming Exercises

--- C/stuffs/temperature-calculation.c
+++ C/stuffs/temperature-calculation.c
@@ -36,10 +36,10 @@ printf("3. Fahrenheit to Celcius\n");
 printf("4. Fahrenheit to Kelvin\n");
 printf("5. Kelvin to Celcius\n");
 printf("6. Kelvin to Fahrenheit\n");
-printf("7. Exit\n");
+printf("7. Exit\n\n");
 printf("Which calculation? 1/2/3/4/5/6/7:");
 scanf("%d", &The_User_Input);
-
+printf("\n\n\n");
 //define the user choice
 switch(The_User_Input)
 {
@@ -74,17 +74,66 @@ break;
 
 case 3:
 
+
+//input the Fahrenheit value for user
+printf("Input the Fahrenheit value:");
+scanf("%f",&Fahrenheit_Input);
+
+//Fahrenheit to Celcius formula and calculation
+Fahrenheit_Result=(Fahrenheit_Input - 32) * 5/9;
+
+//print the result
+printf(" %0.2f Fahrenheit is equal with %0.2f Celcius!\n",Fahrenheit_Input,Fahrenheit_Result);
+
+
 break;
 
 case 4:
+
+
+//input the celcius value for user
+printf("Input the Fahrenheit value:");
+scanf("%f",&Fahrenheit_Input);
+
+//Fahrenheit to Kelvin formula and calculation
+Fahrenheit_Result=(Fahrenheit_Input - 32) * 5/9 + 273.15;
+
+//print the result
+printf(" %0.2f Fahrenheit is equal with %0.2f Kelvin!\n",Fahrenheit_Input,Fahrenheit_Result);
+
+
 
 break;
 
 case 5:
 
+//input the Kelvin value for user
+printf("Input the Kelvin value:");
+scanf("%f",&Kelvin_Input);
+
+//Kelvin to Celcius formula and calculation
+Kelvin_Result=Kelvin_Input - 273.15;
+
+//print the result
+printf(" %0.2f Kelvin is equal with %0.2f Celcius!\n",Kelvin_Input,Kelvin_Result);
+
+
+
+
 break;
 
 case 6:
+
+//input the Kelvin value for user
+printf("Input the Kelvin value:");
+scanf("%f",&Kelvin_Input);
+
+//Kelvin to Fahrenheit formula and calculation
+Kelvin_Result=(Kelvin_Input - 273.15) * 9/5 + 32;
+
+//print the result
+printf(" %0.2f Kelvin is equal with %0.2f Fahrenheit!\n",Kelvin_Input,Kelvin_Result);
+
 
 break;
 
@@ -103,7 +152,6 @@ break;
 
 
 }
-
 
 
 
