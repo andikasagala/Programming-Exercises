414ae11cb4c229decd69b58850b761cfe8bdcc5d
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-02 15:44:09 +0700

    Programming Exercises
    Programming Exercises

---
+++ C/Basic/Keywords-I.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+#############################################################
+#                                                           #
+# ---== Keywords I break,case,switch,default,continue ==--- #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2020                           #
+#############################################################
+*/
+
+int main()
+{
+int i=1;
+
+//sample how to use keywords of switch
+switch(i)
+{
+
+//sample how to use keywords of case
+case 1:
+printf("Hola!\n");
+//sample how to use keywords of break
+break;
+
+//sample how to use keywords of case
+case 2:
+printf("Adios");
+//sample how to use keywords of continue
+continue;
+
+//sample how to use keywords of default
+default:
+printf("none");
+//sample how to use keywords of break
+break;
+}
+
+
+return 0;
+}
