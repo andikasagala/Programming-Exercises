f8e74767e02da9f61dfdbbcb7481a23e2ec5986f
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-04 00:00:50 +0700

    Update Arrays-II.c

--- C/basic/Arrays-II.c
+++ C/basic/Arrays-II.c
@@ -21,7 +21,7 @@ char Arrays_Var2[2][3]={{'P','O','I'},{'Q','A','Z'}};
 
 printf("\n\n---== Multidimensional Arrays in number ==---\n\n");
 printf("\n_____________________________________________________ \n\n");
-//looping for arrays in number version
+//looping  arrays in number version
 printf("_______________");
 printf("\n\nData: {{4,7,3},{2,8,3}} \n");
 printf("_______________\n");
@@ -29,7 +29,7 @@ for(i=0;i<=1;i++)
 {
 for(j=0;j<=2;j++)
 {
-k+=1; //increment for numbering the arrays
+k+=1; //increment for arrays's numbering
 printf("Array %d value is %d\n",k,Arrays_Var1[i][j]); //print the array value
 }
 }
@@ -37,7 +37,7 @@ printf("Array %d value is %d\n",k,Arrays_Var1[i][j]); //print the array value
 printf("\n\n\n_____________________________________________________ \n");
 printf("\n\n---== Multidimensional Arrays in char ==---\n\n");
 printf("\n_____________________________________________________ \n\n");
-//looping for arrays in char version
+//looping arrays in char version
 printf("_______________");
 printf("\n\nData: {{'P','O','I'},{'Q','A','Z'}} \n");
 printf("_______________\n");
@@ -45,7 +45,7 @@ for(l=0;l<=1;l++)
 {
 for(m=0;m<=2;m++)
 {
-n+=1; //increment for char the arrays
+n+=1; //increment for arrays's char
 printf("Array %d value is %c\n",n,Arrays_Var2[l][m]); //print the array value
 }
 }
