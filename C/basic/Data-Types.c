a7137758dcff98410c206ed62744ff7ad0d91bf4
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-03 20:30:21 +0700

    Update Data-Types.c

--- C/basic/Data-Types.c
+++ C/basic/Data-Types.c
@@ -27,6 +27,7 @@ long int Variable_G = 12;
 signed short int Variable_H = 1;
 long long int Variable_I = 2;
 unsigned int Variable_J = 1;
+short int Variable_K=10;
 
 //print the output/values
 printf("Data type 'FLOAT' with value: %f \n", Variable_A);
@@ -38,6 +39,7 @@ printf("Data type 'INT' Long with value: %d \n", Variable_G);
 printf("Data type 'INT' Short with value: %d \n", Variable_H);
 printf("Data type 'INT' long long with value: %d \n", Variable_I);
 printf("Data type 'INT' unsigned with value: %d \n", Variable_J);
+printf("Data type 'INT' unsigned with value: %d \n", Variable_K);
 printf("Total Data Types: Integral (INT), Float (FLOAT), Character (CHAR), and Character String (CHAR)\n\n\n");
 return 0;
 }
