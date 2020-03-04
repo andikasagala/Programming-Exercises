655d61a8927cefabb409c5ec29941a02b3682195
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2020-03-04 07:01:20 +0700

    Create decision-II.c

---
+++ C/basic/decision-II.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+#############################################################
+#                                                           #
+#              ---== Decision II if else ==---              #
+#                                                           #
+#                                                           #
+#               Coder: Andika Sagala                        #
+#               Date: March, 2020                           #
+#############################################################
+*/
+
+int main()
+{
+//declare the variable
+int a=1;
+
+
+//decision using if else 
+if(a==1)
+{
+printf("Hola! \n");
+}
+else
+{
+printf("none..\n");
+}
+
+
+return 0;
+}
